1e77aa787a830a763518a608f547c4b57a52f0d1
Sofia Shevchuk <sofiashevtcuk2.04@gmail.com>
2023-10-12 12:44:39 +0300

    delete unused imports 2x

--- src/test/java/api/clients/AbstractBaseRestClient.java
+++ src/test/java/api/clients/AbstractBaseRestClient.java
@@ -3,15 +3,15 @@ package api.clients;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 
-import java.io.IOException;
 import java.util.Map;
 
-import static api.clients.ConfigProvider.*;
+import static api.clients.ConfigProvider.KEY;
+import static api.clients.ConfigProvider.TOKEN;
 import static io.restassured.RestAssured.given;
 
 public abstract class AbstractBaseRestClient {
     protected final RequestSpecification requestSpec;
-    private final static Map<String, String> KEYANDTOKEN = Map.of("key",KEY,"token", TOKEN);
+    private final static Map<String, String> KEYANDTOKEN = Map.of("key", KEY, "token", TOKEN);
 
     public AbstractBaseRestClient(String url) {
 
--- src/test/java/api/clients/ConfigProvider.java
+++ src/test/java/api/clients/ConfigProvider.java
@@ -6,8 +6,8 @@ import com.typesafe.config.ConfigFactory;
 public interface ConfigProvider {
     Config config = readConf();
 
-    static Config readConf(){
-        return  ConfigFactory.load("application.conf");
+    static Config readConf() {
+        return ConfigFactory.load("application.conf");
     }
 
     String KEY = readConf().getString("key");
