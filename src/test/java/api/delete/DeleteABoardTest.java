601e6ede932b048f38bfe373f0a1d18c4c58a222
Safron1ta2 <elsafronitaz@gmail.com>
2023-10-13 14:52:55 +0300

    test refactoring

--- src/test/java/api/delete/DeleteABoardTest.java
+++ src/test/java/api/delete/DeleteABoardTest.java
@@ -1,45 +1,36 @@
 package api.delete;
 
 import api.BaseTest;
-import api.clients.BoardRestTestClient;
 import api.dto.CreateBoardResponse;
 import io.restassured.response.ValidatableResponse;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static api.Const.BASE_URL;
 import static java.net.HttpURLConnection.HTTP_NOT_FOUND;
-import static java.net.HttpURLConnection.HTTP_OK;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class DeleteABoardTest extends BaseTest {
 
-    private BoardRestTestClient boardRestTestClient = new BoardRestTestClient(BASE_URL);
-    private String ID_BOARD;
+    private String bordId;
 
-    @Test
-    public void deleteBoardTest() {
+    @BeforeClass
+    public void setup() {
         Map<String, String> boardParams = new HashMap<>();
         boardParams.put("name", "Test board");
 
         CreateBoardResponse response = boardRestTestClient.createNewBoard(boardParams);
-        ID_BOARD = response.getId();
+        bordId = response.getId();
+    }
 
-        ValidatableResponse deleteResponse = boardRestTestClient.deleteBoardIfExist(ID_BOARD);
+    @Test
+    public void deleteBoardTest() {
+        boardRestTestClient.deleteBoard(bordId);
 
-        int statusCode = deleteResponse.extract().statusCode();
-        Assert.assertTrue(statusCode == HTTP_OK || statusCode == HTTP_NOT_FOUND, "Unexpected status code");
-
-        Boolean boardWasFound = deleteResponse.extract().body().path("found");
-        if (boardWasFound != null) {
-            Assert.assertTrue(boardWasFound, "Board was not found");
-        }
-
-        String boardName = deleteResponse.extract().body().path("name");
-        if (boardName != null) {
-            Assert.assertEquals("Test board", boardName, "Board name does not match");
-        }
+        String message = boardRestTestClient.getBoardById(bordId, HTTP_NOT_FOUND).asString();
+        assertThat(message).isEqualTo("The requested resource was not found.");
     }
 }
