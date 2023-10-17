cb6d54915719125809bc9e7c8e79ec1309439134
Goldbe54 <dimon54royko@gmail.com>
2023-10-17 15:24:58 +0300

    AS2-24 refactor code

--- src/test/java/api/post/CreateAChecklist.java
+++ src/test/java/api/post/CreateAChecklist.java
@@ -9,6 +9,7 @@ import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
@@ -28,7 +29,7 @@ public class CreateAChecklist extends BaseTest {
     private String listId;
     private String cardId;
 
-    @BeforeClass
+    @BeforeMethod
     @Step("Fulfillment of the prerequisites for the test")
     public void preconditions() {
         boardId = boardRestTestClient.createNewBoard(constructDefaultBoardKeyValue()).getId();
