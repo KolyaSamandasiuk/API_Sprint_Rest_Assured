8cb735c0ec4dc180cec720c8391b98b91c926689
DashaYab <dash.yab@yahoo.com>
2023-10-17 15:43:33 +0300

    Add changes in GerACardInfoTest

--- src/test/java/api/get/GetACardInfoTest.java
+++ src/test/java/api/get/GetACardInfoTest.java
@@ -4,7 +4,7 @@ import api.BaseTest;
 import api.dto.CardDataResponse;
 import jdk.jfr.Description;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -36,7 +36,7 @@ public class GetACardInfoTest extends BaseTest {
         Assert.assertEquals(DESC_VALUE, response.getDesc());
     }
 
-    @AfterClass
+    @AfterMethod
     public void deleteBoard() {
         cardTestRestClient.deleteCardIfExist(idCard);
     }
